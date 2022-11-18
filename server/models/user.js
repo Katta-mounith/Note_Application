ea0b9eecf27e32750457c48d75e33b41383ff8e0
Katta-mounith <54463229+Katta-mounith@users.noreply.github.com>
2022-11-18 12:11:29 -0500

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -1,1 +1,32 @@
-
+// "database" as object literal
+const users = [
+    {
+      
+      userName: "potter",
+      Password: "adfh"
+    },
+    {
+     
+      userName: "harry",
+      Password: "badv"
+    },
+    {
+     
+      userName: "Mounith",
+      Password: "hello"
+    }
+  ];
+  
+  function getAllUsers() {
+    return users;
+  }
+  function login(user) { // {userName: "sda", password: "gsdhjsga"}
+    let cUser = users.filter( u => u.userName === user.userName);
+    
+    if(!cUser[0]) throw Error("Username not found");
+    if(cUser[0].password !== user.password) throw Error("Password incorrect");
+  
+    return cUser[0];
+  }
+  
+  module.exports = { getAllUsers, login };
